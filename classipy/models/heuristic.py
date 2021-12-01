25fb8e0b443cc9711d6b59e487f0b03bd4c4ca65
Marek Sherman <marek.sherman@gmail.com>
2021-12-01 11:22:41 +0100

    Added a funtion to heuristics.py to handel None type

--- classipy/models/heuristic.py
+++ classipy/models/heuristic.py
@@ -4,12 +4,19 @@ import math
 import ast
 import numpy as np
 
+
 class Heuristic ():
 
     def __init__(self) -> None:
         pass
 
-    
+    def fill_nan_none(self,df):
+        for index,row in df.iterrows():
+            for col in ['mean','std','median','skew','kurt','shapiro_wilk_test']:
+                if row[col] == None:
+                    df.loc[index, col] = np.nan
+        return df
+
     def check_cat_binary(self,df):
         df['heuristic_label'] = np.nan
         for index,row in df.iterrows():
@@ -42,7 +49,7 @@ class Heuristic ():
             if 'province' in row['column_name'].lower():
                 df.loc[index,'heuristic_label'] = 2
             elif 'region' in row['column_name'].lower():
-                df.loc[index,'heuristic_label'] = 2  
+                df.loc[index,'heuristic_label'] = 2
             elif 'country' in row['column_name'].lower():
                 df.loc[index,'heuristic_label'] = 2
             elif 'category' in row['column_name'].lower():
@@ -61,8 +68,10 @@ class Heuristic ():
                 df.loc[index,'heuristic_label'] = 2
         return df
 
-    
+
     def check_text(self,df):
+        for col in df.columns:
+            print(col)
         for index,row in df.loc[df.heuristic_label.isnull()].iterrows():
             if math.isnan(row['mean']) and math.isnan(row['std']) and math.isnan(row['median']) and math.isnan(row['skew']) and math.isnan(row['kurt']) and (row.n_unique_values/row.n_values) > 0.8 :
                 df.loc[index,'heuristic_label'] = 4
@@ -70,6 +79,8 @@ class Heuristic ():
 
 
     def check_int_float(self,df):
+        for col in df.columns:
+            print(col)
         for index,row in df.loc[df.heuristic_label.isnull()].iterrows():
             if math.isnan(row['mean'])==False and math.isnan(row['std'])==False and math.isnan(row['median'])==False and math.isnan(row['skew'])==False and math.isnan(row['kurt'])==False and (row.n_unique_values/row.n_values) > 0.8:
                 list_n_uniques = ast.literal_eval(row.column_values_unique)
@@ -79,16 +90,17 @@ class Heuristic ():
                     df.loc[index,'heuristic_label'] = 1
         return df
 
-    
+
     def test_dataset_heuristic(self,df):
-            df = self.check_cat_binary(df)
-            df = self.check_date(df)
-            df = self.check_cat_multi(df)
-            df= self.check_text(df)
-            df= self.check_int_float(df)
-            df.loc[df.heuristic_label.isnull()] = 3
-            #df = df_bin.append([df_cat_multi,df_text,df_int_float,df],ignore_index=True)
-            return df.heuristic_label
+        df = self.fill_nan_none(df)
+        df = self.check_cat_binary(df)
+        df = self.check_date(df)
+        df = self.check_cat_multi(df)
+        df= self.check_text(df)
+        df= self.check_int_float(df)
+        df.loc[df.heuristic_label.isnull()] = 3
+        #df = df_bin.append([df_cat_multi,df_text,df_int_float,df],ignore_index=True)
+        return df.heuristic_label
 
 if __name__ == '__main__':
     df = pd.read_csv('../../raw_data/kaggle_data_merged_with_labels.csv')
@@ -97,6 +109,3 @@ if __name__ == '__main__':
     df = heuristic.test_dataset_heuristic(df)
     print(df.shape)
     print(df)
-
-    
-
